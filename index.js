aeb1e22b5f21412aed7913e2d7732d7162d9236e
if1live <libsora25@gmail.com>
2018-02-26 18:57:18 +0900

    3-step decrypt

--- index.js
+++ index.js
@@ -1,29 +1,58 @@
 const aesjs = require('aes-js');
-const key = require('./static/key');
 
-const aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(5));
+// shared variable
+var encryptedBytes = null;
+var encKey = null;
 
-document.querySelector('.btn-download-and-decrypt').onclick = function() {
-  let url = '/media-encrypt/sample.gif.enc';
+document.querySelector('.btn-fetch-enc').onclick = function() {
+  var btn = this;
+
+  let url = '/static/media-encrypt/sample.gif.enc';
   fetch(url).then(function(resp) {
     return resp.blob();
   }).then(function(blob) {
     let reader = new FileReader();
     reader.addEventListener('loadend', function() {
       let view = new Uint8Array(this.result);
-      let bytes = aesCtr.decrypt(view);
-      let el = document.querySelector('#img-dst');
-      applyImage(el, 'image/gif', bytes);
+      encryptedBytes = view;
+
+
+      console.log('fetch enc complete');
+      btn.disabled = true;
     });
     reader.readAsArrayBuffer(blob);
   });
 };
 
+document.querySelector('.btn-fetch-key').onclick = function() {
+  var btn = this;
+
+  let url = '/static/key.json';
+  fetch(url).then(function(resp) {
+    return resp.json();
+  }).then(function(json) {
+    encKey = json;
+
+    console.log('fetch key complete');
+    btn.disabled = true;
+  });
+}
+
+document.querySelector('.btn-decrypt').onclick = function() {
+  var btn = this;
+
+  const aesCtr = new aesjs.ModeOfOperation.ctr(encKey, new aesjs.Counter(5));
+  let bytes = aesCtr.decrypt(encryptedBytes);
+  let el = document.querySelector('#img-dst');
+  applyImage(el, 'image/gif', bytes);
+
+  btn.disabled = true;
+}
+
 function applyImage(el, mimetype, bytes) {
   var base64String = btoa(
     bytes.reduce((data, byte) => data + String.fromCharCode(byte), '')
   );
-
   let base64 = `data:${mimetype};base64,` + base64String;
   el.src = base64;
 }
