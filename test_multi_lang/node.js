36f6ca1aa7d7f5b3a9a4ed4e0e11f4b9adafc624
if1live <libsora25@gmail.com>
2018-03-06 12:51:32 +0900

    use crypto-js

--- test_multi_lang/node.js
+++ test_multi_lang/node.js
@@ -1,28 +1,35 @@
+
 const config = require('./config');
-const aesjs = require('aes-js');
+const CryptoJS = require('crypto-js');
 
-// Convert text to bytes
-var textBytes = aesjs.utils.utf8.toBytes(config.text);
+var text = config.text;
 
-// The counter is optional, and if omitted will begin at 1
-var aesCtr = new aesjs.ModeOfOperation.ctr(config.key, new aesjs.Counter(5));
-var encryptedBytes = aesCtr.encrypt(textBytes);
+function run_text_with_aes_128_ctr(text) {
+  // prepare config
+  let counter = new Uint8Array(16);
+  counter[15] = 5;
+  let iv = CryptoJS.lib.WordArray.create(counter);
 
-// To print or store the binary data, you may convert it to hex
-var encryptedHex = aesjs.utils.hex.fromBytes(encryptedBytes);
-console.log(encryptedHex);
-// "a338eda3874ed884b6199150d36f49988c90f5c47fe7792b0cf8c7f77eeffd87
-//  ea145b73e82aefcf2076f881c88879e4e25b1d7b24ba2788"
+  let key = CryptoJS.lib.WordArray.create(new Uint8Array(config.aesKey));
 
-// When ready to decrypt the hex string, convert it back to bytes
-var encryptedBytes = aesjs.utils.hex.toBytes(encryptedHex);
+  let cfg = {
+    mode: CryptoJS.mode.CTR,
+    padding: CryptoJS.pad.NoPadding,
+    iv: iv,
+  };
 
-// The counter mode of operation maintains internal state, so to
-// decrypt a new instance must be instantiated.
-var aesCtr = new aesjs.ModeOfOperation.ctr(config.key, new aesjs.Counter(5));
-var decryptedBytes = aesCtr.decrypt(encryptedBytes);
+  // text -> encrypt
+  let ciphertext = CryptoJS.AES.encrypt(text, key, cfg);
+  let encrypted = CryptoJS.format.Hex.stringify(ciphertext);
+  console.log(encrypted);
 
-// Convert our bytes back into text
-var decryptedText = aesjs.utils.utf8.fromBytes(decryptedBytes);
-console.log(decryptedText);
-// "Text may be any length you wish, no padding is required."
+  // encrypted text -> decrypt
+  let cipher = {
+    ciphertext: CryptoJS.enc.Hex.parse(encrypted),
+  };
+  let bytes  = CryptoJS.AES.decrypt(cipher, key, cfg);
+  let plaintext = bytes.toString(CryptoJS.enc.Utf8);
+  console.log(plaintext);
+}
+
+run_text_with_aes_128_ctr(text);
--- test_multi_lang/python.py
+++ test_multi_lang/python.py
@@ -4,29 +4,37 @@ from Crypto.Cipher import AES
 from Crypto.Util import Counter
 import binascii
 
+
 def load_config():
     fp = os.path.join(os.path.dirname(__file__), 'config.json')
     f = open(fp)
     return json.load(f)
 
 config = load_config()
-hex_str = ' '.join(['{0:02X}'.format(x) for x in config['key']])
-key = bytes(bytearray.fromhex(hex_str))
 text = config['text']
 
-def create_cipher(key):
+def create_aes_128_ctr_cipher():
+    hex_str = ' '.join(['{0:02X}'.format(x) for x in config['aesKey']])
+    key = bytes(bytearray.fromhex(hex_str))
+
     counter_nbit = 128
     counter_initial_value = 5
     ctr = Counter.new(counter_nbit, initial_value=counter_initial_value)
     cipher = AES.new(key, AES.MODE_CTR, counter=ctr)
     return cipher
 
-# encrypt
-cipher = create_cipher(key)
-ct = cipher.encrypt(text)
-print(binascii.hexlify(ct).decode('utf-8'))
+def run_text_with_aes_128_ctr(text):
+    # encrypt
+    cipher = create_aes_128_ctr_cipher()
+    ct = cipher.encrypt(text.encode('utf-8'))
+    print(binascii.hexlify(ct).decode('utf-8'))
 
-# decrypt. do not reuse cipher
-cipher = create_cipher(key)
-text = cipher.decrypt(ct)
-print(text.decode('utf-8'))
+    # decrypt. do not reuse cipher
+    cipher = create_aes_128_ctr_cipher()
+    text = cipher.decrypt(ct)
+    print(text.decode('utf-8'))
+
+
+run_text_with_aes_128_ctr(text)
+
+
